9ab4887c70b4f4793a50685e0f8281a3aed909dc
Dmitry Zhurbin <dante102@mail.ru>
2025-08-17 02:41:32 +0300

    Add 2 homework 5 lession

---
+++ src/test/java/testSelenideHomeWork/TestHoverGithubLess5.java
@@ -0,0 +1,28 @@
+package testSelenideHomeWork;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestHoverGithubLess5 {
+    @BeforeAll
+    static void setupEnvironment () {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://github.com";
+    }
+
+    @Test
+    void hoverToSolutionsAndClickEnterprizeTest(){
+        open("");
+        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+        $("a[href*='enterprise']").click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered")).shouldHave(text("developer platform"));
+    }
+}
+
+
---
+++ src/test/java/testSelenideHomeWork/TestRectangleTransferLess5.java
@@ -0,0 +1,44 @@
+package testSelenideHomeWork;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestRectangleTransferLess5 {
+    @BeforeAll
+    static void setupEnvironment () {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://the-internet.herokuapp.com/drag_and_drop";
+    }
+
+    @Test
+    void TestRectangleTransferSelenideAction(){
+        open("");
+        SelenideElement elementA = $("#column-a");
+        SelenideElement elementB = $("#column-b");
+        actions()
+                .clickAndHold(elementA)
+                .moveToElement(elementB)
+                .release()
+                .perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    void TestRectangleTransferDragAndDrop(){
+        open("");
+        $("#column-a").dragAndDrop((DragAndDropOptions) DragAndDropOptions.to(Selenide.$("#column-b")));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+}
