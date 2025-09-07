8b11d444da426562b60847f7b36f3a6a44d1cd03
Dmitry Zhurbin <dante102@mail.ru>
2025-09-08 02:41:07 +0300

    Подтянул изменения после изменений структуры папок

--- src/test/java/pages/components/less6/CheckFillFormComponentLess6.java
+++ src/test/java/pages/components/less6/CheckFillFormComponentLess6.java
@@ -1,24 +1,23 @@
-package pages.components;
+package pages.components.less6;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.*;
 
-public class CheckFillFormComponentLess7 {
+public class CheckFillFormComponentLess6 {
     private final SelenideElement
             modalDialog = $(".modal-dialog"),
             formTable   = $(".table-responsive");
 
-    public CheckFillFormComponentLess7 checkTable(String key, String value) {
+    public CheckFillFormComponentLess6 checkTable(String key, String value) {
         modalDialog.should(appear);
         formTable.$(byText(key)).parent().shouldHave(text(value));
         return null;
     }
 
-    public CheckFillFormComponentLess7 modalDialogNotAppear() {
+    public CheckFillFormComponentLess6 modalDialogNotAppear() {
         modalDialog.shouldNot(appear);
         return null;
     }
