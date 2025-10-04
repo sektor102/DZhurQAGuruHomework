c8cee6a01015b116d8d73d8ae0a175615072a8f6
Dmitry Zhurbin <dante102@mail.ru>
2025-10-04 13:18:00 +0300

    Поправил степы, хоть требования такого и не было, как и внятного объяснения почему именно так нужно делать

--- src/test/java/guru/qa/dZhurHomeWork/less10_8_Allure/TestCheckIssueStep.java
+++ src/test/java/guru/qa/dZhurHomeWork/less10_8_Allure/TestCheckIssueStep.java
@@ -2,51 +2,30 @@ package guru.qa.dZhurHomeWork.less10_8_Allure;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.Step;
+import guru.qa.dZhurHomeWork.less10_8_Allure.baseTest10_8.WebStepHomeWork;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
 public class TestCheckIssueStep {
 
+    public static final String baseUrl = "https://github.com";
     public static final String repository = "/qa-guru/allure-notifications";
     public static final int issue = 327;
 
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
         SelenideLogger.addListener("allure", new AllureSelenide());
-
-    }
-
-    @Step("Открываем страницу репозитория" + repository)
-    public void openRepoPage() {
-        open(repository);
-    }
-
-    @Step("Жмем по кнопке Issue")
-    public void clickIssueButton() {
-        $("#issues-tab").click();
-    }
-
-    @Step("Проверяем что есть " + issue + " вопрос")
-    public void checkIssueNumber() {
-        $("[data-listview-component=items-list]")
-                .should(appear)
-                .shouldHave(text("#327"));
     }
 
     @Test
     public void testStepCheckIssue() {
-        openRepoPage();
-        clickIssueButton();
-        checkIssueNumber();
+        WebStepHomeWork steps = new WebStepHomeWork(baseUrl, repository, issue);
+
+        steps.openRepoPage();
+        steps.clickIssueButton();
+        steps.checkIssueNumber();
     }
 }
